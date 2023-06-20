7e0e2e6e8f15eacc8b93bf7ffe7bc558ea6262b2
Sperling, Nicholas Niven <Nicholas.Sperling@utoledo.edu>
2023-06-20 09:12:08 -0400

    added dosetools to full import.

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from .case_comment_data import *  # noqa: W401, W611
 from .couchtop import *           # noqa: W401, W611
 from .points import *             # noqa: W401, W611
 from .rsdicomread import *        # noqa: W401, W611
+from .dosetools import *        # noqa: W401, W611
 
 del logging                       # noqa: E602
 
