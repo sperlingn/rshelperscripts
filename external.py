2acc74ffd5c3a6f71366f3dee52146393efc0186
Sperling, Nicholas Niven <Nicholas.Sperling@utoledo.edu>
2023-07-27 16:59:40 -0400

    Add a few messagebox wrappers and indicators that RS functions have been overridden

--- external.py
+++ external.py
@@ -1,10 +1,21 @@
+from enum import IntEnum
 import logging as _logging
 _logger = _logging.getLogger(__name__)
+
+
+def helperoverride(function):
+    function.__overridden__ = True
+    return function
 
 
 try:
     from System.Windows import MessageBox as _MessageBox
 except ImportError:
+
+    # TODO: Work to include a QT or other based dialog? Right now we can assume
+    # that the Windows MessageBox will work for RS.  This is only for debugging
+    # functions.
+    @helperoverride
     class _MessageBox:
         def Show(*args, **kwargs):
             _logger.info("MessageBox: args={}, kwargs={}", args, kwargs)
@@ -31,6 +42,92 @@ class _CompositeActionDummy():
         _logger.info("Exited  {} Undo state.".format(self._name))
 
 
+class MB_Button(IntEnum):
+    OK = 0
+    OKCancel = 1
+    YesNo = 4
+    YesNoCancel = 3
+
+
+class MB_Icon(IntEnum):
+    Asterisk = 64
+    Error = 16
+    Exclamation = 48
+    Hand = 16
+    Information = 64
+    None_ = 0
+    Question = 32
+    Stop = 16
+    Warning_ = 48
+
+
+class MB_Result(IntEnum):
+    Cancel = 2
+    No = 7
+    None_ = 0
+    OK = 1
+    Yes = 6
+
+
+class MB_Options(IntEnum):
+    DefaultDesktopOnly = 131072
+    None_ = 0
+    RightAlign = 524288
+    RtlReading = 1048576
+    ServiceNotification = 2097152
+
+
+def Show_OK(message, caption, ontop=False, icon=MB_Icon.None_,
+            defaultResult=MB_Result.None_):
+    button = MB_Button.OK
+    opt = MB_Options.DefaultDesktopOnly if ontop else MB_Options.None_
+    return _MessageBox.Show(message, caption, button, icon, defaultResult, opt)
+
+
+def Show_OKCancel(message, caption, ontop=False, icon=MB_Icon.None_,
+                  defaultResult=MB_Result.None_):
+    button = MB_Button.OKCancel
+    opt = MB_Options.DefaultDesktopOnly if ontop else MB_Options.None_
+    return _MessageBox.Show(message, caption, button, icon, defaultResult, opt)
+
+
+def Show_YesNo(message, caption, ontop=False, icon=MB_Icon.None_,
+               defaultResult=MB_Result.None_):
+    button = MB_Button.YesNo
+    opt = MB_Options.DefaultDesktopOnly if ontop else MB_Options.None_
+    return _MessageBox.Show(message, caption, button, icon, defaultResult, opt)
+
+
+def Show_YesNoCancel(message, caption, ontop=False, icon=MB_Icon.None_,
+                     defaultResult=MB_Result.None_):
+    button = MB_Button.YesNoCancel
+    opt = MB_Options.DefaultDesktopOnly if ontop else MB_Options.None_
+    return _MessageBox.Show(message, caption, button, icon, defaultResult, opt)
+
+
+def _await_user_input_mb(message):
+    _logger.debug("Waited for user input: '{}'".format(message))
+    return _MessageBox.Show("{}".format(message), "Awaiting Input",
+                            # Always on top = 131072
+                            1, 0, 0, 131072)
+
+
+try:
+    from connect import RayWindow
+except ImportError:
+    try:
+        import wpf
+        from System.Windows import Window
+
+        class RayWindow(Window):
+            def LoadComponent(self, XAML):
+                return wpf.LoadComponent(self, XAML)
+    except ImportError:
+        class RayWindow():
+            def LoadComponent(self, XAML):
+                raise NotImplementedError("Neither RS nor wpf in environment")
+
+
 try:
     from connect import (get_current, CompositeAction as _CompositeActionOrig,
                          await_user_input as _await_user_input)
@@ -46,11 +143,11 @@ except ImportError:
     class _CompositeActionOrig(_CompositeActionDummy):
         pass
 
-    def _await_user_input(msg):
-        _logger.logging("Waited for user input: '{}'".format(msg))
+    _await_user_input = _await_user_input_mb
 
 finally:
 
+    @helperoverride
     class CompositeAction:
         _clsinstance = None
         _instance = None
@@ -77,12 +174,14 @@ finally:
             rv = type(self._instance).__exit__(self._instance,
                                                e_type, e, e_traceback)
 
+            # FIXME:
             # Make sure that we don't reuse this object later (for now we can
             # only enter and exit once...with more logic this could be fixed.
             self._instance = None
 
             return rv
 
+    @helperoverride
     def await_user_input(msg):
         # Wrapper to prevent await_user_input from spawning during a composite
         # action context manager.
@@ -92,18 +191,16 @@ finally:
             _logger.warning("Tried to call await_user_input during a composite"
                             " action.  This leads to a crash. Ignoring call.\n"
                             "Message was: '{}'".format(msg), exc_info=True)
-            return _MessageBox.Show("{}".format(msg), "Awaiting Input",
-                                    # Always on top = 131072
-                                    1, 0, 0, 131072)
-
+            _await_user_input_mb(msg)
         else:
-            return _await_user_input(msg)
+            _await_user_input(msg)
 
 try:
     from pydicom import dcmread
 except ImportError:
     # If we don't have pydicom in this env, can't do any of this. Just return a
     # dummy function that returns none.
+    @helperoverride
     def dcmread(*args, **kwargs):
         return None
 
