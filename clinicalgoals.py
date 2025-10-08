0835a576ae5d2760ad21d6b1637aeb8da4b1fcd3
Sperling, Nicholas Niven <Nicholas.Sperling@utoledo.edu>
2025-10-08 13:33:57 -0400

    add ClinicalGoals class

--- clinicalgoals.py
+++ clinicalgoals.py
@@ -86,3 +86,59 @@ def copy_clinical_goals(plan_in, plan_out):
     _logger.debug(f"Copying goals: {uniq_fns}")
     for fn_params in uniq_fns:
         evalsetup_out.AddClinicalGoal(**fn_params)
+
+
+# Reduced and copied from evalfns class in rshelperscripts
+class ClinicalGoal():
+    _fn = None
+    evals = None
+    # This will be much easier with template strings when we can move to
+    # python 3.14...159265
+    GoalCriteria = {'AtLeast': '>',
+                    'AtMost': '<'}
+    GoalStrings = {
+        'AverageDose': '_Dmean {goaldir} {al}cGy: {value}cGy',
+        'VolumeAtDose': '_V{pv}cGy {goaldir} {al:.2%}: {value:.2%}',
+        'DoseAtVolume': '_D{pv:.0%} {goaldir} {al}cGy: {value}cGy',
+        'DoseAtPoint': '_D {goaldir} {al}cGy: {value}cGy',
+        'AbsoluteVolumeAtDose': '_V{pv}cGy {goaldir} {al}cc: {value}cc',
+        'DoseAtAbsoluteVolume': '_D{pv}cc {goaldir} {al}cGy: {value}cGy',
+        'ConformityIndex': '_CI {goaldir} {al}: {value}',
+        'HomogeneityIndex': '_HI {goaldir} {al}: {value}'
+    }
+
+    def __init__(self, rs_dose_eval_fn, dose=None):
+        self._fn = rs_dose_eval_fn
+        self._gcgvfed = self._fn.GetClinicalGoalValueForEvaluationDose
+        self.evals = {}
+
+        if dose:
+            self.eval_dose(dose)
+
+    def eval_dose(self, dose, scaledose=False):
+        eval_dose = self._gcgvfed(DoseDistribution=dose,
+                                  ScaleFractionDoseToBeamSet=scaledose)
+        self.evals[dose.Name] = eval_dose
+        return eval_dose
+
+    @property
+    def name(self):
+        if self._fn.ForRegionOfInterest:
+            return self._fn.ForRegionOfInterest.Name
+        elif self._fn.OfPoiGeometry:
+            return self._fn.OfPoiGeometry.OfPoi.Name
+
+    def __str__(self):
+        # Do the pretty printing
+        pg = self._fn.PlanningGoal
+        params = {'pv': getattr(pg, 'ParameterValue', None),
+                  'al': getattr(pg, 'AcceptanceLevel', None),
+                  'goaldir': self.GoalCriteria[pg.GoalCriteria]}
+
+        strs = {dosename: self.GoalStrings[pg.Type].format(value=val, **params)
+                for dosename, val in self.evals.items()}
+
+        if len(strs) == 1:
+            return f'{self.name}{next(iter(strs.values()))}'
+        else:
+            return '\n'.join(f'{dn}: {self.name}{strs[dn]}' for dn in strs)
